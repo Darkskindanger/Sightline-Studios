4518917286adb25070a00a0ffdfeb844249e11be
Darkskindanger <kiliantavareswork@gmail.com>
2024-09-11 22:27:50 +1000

    Sightline v1

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a" }
+      },
+      dropShadow: {
+        glow: [
+          "0 0px 20px rgba(255,255, 255, 0.35)",
+          "0 0px 65px rgba(255, 255,255, 0.2)"
+        ]
+      }
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+
+}
